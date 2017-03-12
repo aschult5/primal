ea7b2e77d9bed233b972127c4b4437802e5ee1ee
Andrew Schultz <aschult5@gmail.com>
2017-03-12 16:36:07 -0700

    Use autotools

--- default.nix
+++ default.nix
@@ -3,19 +3,13 @@ with import <nixpkgs> {};
 stdenv.mkDerivation rec {
    name = "primal";
    src = ./.;
-   buildInputs = [ rsync boost163 ];
+   buildInputs = [ boost163 autoconf autoconf-archive automake ];
 
-   unpackPhase = ''
-      rsync -a ${src}/. ./
-      chmod ug+w -R .
+   preConfigure = ''
+      ./bootstrap
    '';
 
-   buildPhase = ''
-      ${gcc}/bin/g++ -Wall -Werror -std=c++14 -Iinclude/ src/primal.cxx src/client.cxx src/server.cxx -o primal -lboost_program_options -lboost_system
-   '';
-
-   installPhase = ''
-      mkdir -p $out/bin
-      rsync -a primal $out/bin
+   configureFlags = ''
+      --with-boost-libdir=${boost163}/lib
    '';
 }
